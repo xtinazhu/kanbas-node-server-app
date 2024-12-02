51c5bbc33e543321f73786d67e97c734a732ae75
Christina Z <zhu.zhiyi@northeastern.edu>
2024-12-02 02:20:56 -0500

    first commit

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ import ModuleRoutes from "./Kanbas/Module/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 const app = express(); // create a new express instance
-
-app.use(
-    cors({
-             credentials: true,
-             origin: process.env.NETLIFY_URL || "http://localhost:3000",
-         })
-);
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
@@ -31,14 +24,21 @@ if (process.env.NODE_ENV !== "development") {
     };
 }
 app.use(session(sessionOptions));
-
 app.use(express.json());
+app.use(
+    cors({
+             credentials: true,
+             origin: process.env.NETLIFY_URL || "http://localhost:3000",
+         })
+);
 
+
+UserRoutes(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+AssignmentRoutes(app);
+EnrollmentRoutes(app);
 Lab5(app);
 Hello(app); // pass app reference to Hello
-UserRoutes(app);
-CourseRoutes(app);
-EnrollmentRoutes(app);
-ModuleRoutes(app);
-AssignmentRoutes(app);
+
 app.listen(process.env.PORT || 4000) // Listen to http://localhost:4000
